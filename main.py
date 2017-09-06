25578c8dd6b726619243bfed06c90df4b9e7aa3b
baranski <jakub.baranski@profil-software.com>
2017-09-06 10:15:55 +0200

    fixed hardcoded access token, saving and opening menu in gedit

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ import json
 from subprocess import call
 import requests
 
-TOKEN = json.loads(open('config.json', 'r').read())
+with open('config.json', 'r') as config_file:
+    TOKEN = json.loads(config_file.read())['access_token']
 
 
 def send_notification():
@@ -14,12 +15,13 @@ def send_notification():
 
 def get_feed():
     response = requests.get(
-        'https://graph.facebook.com/v2.10/1735305803424923/feed?access_token=268353497010712%7CkGpy1aemxt3oA81WMW-QEUvfGuU'
-    )
+        'https://graph.facebook.com/v2.10/1735305803424923/feed?access_token={}'.format(TOKEN ))
     post = response.json()['data'][0]
     post_time = datetime.datetime.strptime(post['created_time'], '%Y-%m-%dT%H:%M:%S%z')
     if post_time.date() == datetime.datetime.now().date():
         if 'tu zjesz' in post['message'].lower():
+            with open('menu.txt', 'w') as menu_file:
+                menu_file.write(post['message'])
             send_notification()
             sys.exit(0)
     sys.exit(1)
