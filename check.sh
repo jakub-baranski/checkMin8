182285aab6cfbbf457f70c4e1517796df13a46a3
baranski <jakub.baranski@profil-software.com>
2017-09-07 13:31:58 +0200

    fixes

--- check.sh
+++ check.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
+export DISPLAY=:0
+cd /home/kuba/Documents/projects/checkMin8
+echo 'Activating venv'
 . venv/bin/activate
 
-START=$(date +%s)
+START=$(/bin/date +%s)
 while ! python3 main.py; do
 	DIFF=$(( $(date +%s) - $START ))
+	echo 'In a loop for ', $DIFF, ' seconds'
 	if [ $DIFF -ge 3600 ]
 	then
 		break
 	fi
     sleep 15
 done
+echo 'got menu'
+echo 'deactivating venv'
 deactivate
-gedit menu.txt
+echo 'running gedit'
+gedit /tmp/menu.txt
--- main.py
+++ main.py
@@ -5,21 +5,21 @@ import json
 
 from subprocess import call
 
+
 with open('config.json', 'r') as config_file:
     TOKEN = json.loads(config_file.read())['access_token']
-
 
 def send_notification():
     call(['notify-send', 'MENUSY! MENUSY! MENUSY! MENUSY! MENUSY!', '--urgency=critical'])
 
 def get_feed():
     response = requests.get(
-        'https://graph.facebook.com/v2.10/1735305803424923/feed?access_token={}'.format(TOKEN ))
+        'https://graph.facebook.com/v2.10/1735305803424923/feed?access_token={}'.format(TOKEN))
     post = response.json()['data'][0]
     post_time = datetime.datetime.strptime(post['created_time'], '%Y-%m-%dT%H:%M:%S%z')
     if post_time.date() == datetime.datetime.now().date():
         if 'tu zjesz' in post['message'].lower():
-            with open('menu.txt', 'w') as menu_file:
+            with open('/tmp/menu.txt', 'w') as menu_file:
                 menu_file.write(post['message'])
             send_notification()
             sys.exit(0)
